13cb6842edb2f750c267859eadfcb5ef9664d255
Ian Kirkpatrick <ianmann56@gmail.com>
2018-03-24 14:13:34 -0400

    Haha

---
+++ src/algorithm/ArrayFileProcessor.java
@@ -0,0 +1,24 @@
+/**
+ * @TODO: TODO
+ *
+ * @author Ian
+ * Created: Mar 24, 2018
+ */
+package algorithm;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+/**
+ * @TODO: TODO
+ *
+ * @author Ian
+ * Created: Mar 24, 2018
+ *
+ */
+public class ArrayFileProcessor {
+
+	public ArrayList<BigDecimal> processArray(ArrayList<BigDecimal> original) {
+		return null;
+	}
+}
